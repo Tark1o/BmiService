3b81de7a2a3ff0112e76c4320c28165de864bebb
Tarkio <wowsendarpm@gmail.com>
2024-02-20 21:01:24 +0300

    autoformatcode

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-public static void main(String[] args) {
-    BmiService service = new BmiService();
-    int weight = 98;
-    double height = 1.87;
-    int index = (int) service.calculate(height, weight);
-    System.out.println(index);
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98;
+        double height = 1.87;
+        int index = (int) service.calculate(height, weight);
+        System.out.println(index);
 
-
-}
+    }
 }
